1424b4e97e8bdb51c98a6c56bcae47a17d5c5f1c
Christopher Robin <williamchen1506@gmail.com>
2024-03-14 21:14:24 +0700

    refactor  : LoadEnv on main.go, deleted config.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,13 +4,20 @@ import (
 	"INTERN_BCC/internal/handler/rest"
 	"INTERN_BCC/internal/repository"
 	"INTERN_BCC/internal/service"
-	"INTERN_BCC/pkg/config"
 	"INTERN_BCC/pkg/database/postgres"
 	"INTERN_BCC/pkg/middleware"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	config.LoadEnv()
+	err := godotenv.Load("../../.env")
+	env := os.Getenv("env")
+	if err != nil && env == "" {
+		log.Fatalf("Failed to load env, err : %v", err)
+	}
+
 	db := postgres.ConnectToDB()
 
 	repository := repository.NewRepository(db)
