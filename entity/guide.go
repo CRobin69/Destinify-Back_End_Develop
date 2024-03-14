2fd77d9a52ff21e07bc8865cabe4d5a0d637c767
Christopher Robin <williamchen1506@gmail.com>
2024-03-14 19:20:23 +0700

    feat : Add Guide entity

--- entity/guide.go
+++ entity/guide.go
@@ -7,12 +7,17 @@ import (
 )
 
 type Guide struct {
-	ID        uuid.UUID `json:"id" gorm:"type:varchar(36);primary_key;"`
-	UserID    uuid.UUID `json:"userid" gorm:"type:varchar(36);primary_key;foreignkey:ID;references:users;onUpdate:CASCADE;onDelete:CASCADE"`
-	Name      string    `json:"name" gorm:"type:varchar(36);not null;"`
-	Price     int       `json:"price" gorm:"type:int;not null;"`
-	PlaceID   uuid.UUID `json:"placeid" gorm:"type:varchar(36);primary_key;foreignkey:ID;references:places;onUpdate:CASCADE;onDelete:CASCADE"`
-	PaymentID uuid.UUID `json:"paymentid" gorm:"type:varchar(36);primary_key;foreignkey:ID;references:payments;onUpdate:CASCADE;onDelete:CASCADE"`
-	CreatedAt time.Time `json:"createdAt" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
+	ID           uuid.UUID `json:"id" gorm:"type:varchar(36);primary_key;"`
+	Name         string    `json:"name" gorm:"type:varchar(36);not null;"`
+	UserID       uuid.UUID `json:"userid" gorm:"type:varchar(36);foreignkey:ID;references:users;onUpdate:CASCADE;onDelete:CASCADE"`
+	PlaceID      uint      `json:"placeid" gorm:"foreignkey:ID;references:places;onUpdate:CASCADE;onDelete:CASCADE"`
+	PaymentID    uuid.UUID `json:"paymentid" gorm:"type:varchar(36);foreignkey:ID;references:payments;onUpdate:CASCADE;onDelete:SET NULL"`
+	GuideDesc    string    `json:"guide_desc" gorm:"type:varchar(1200);not null;"`
+	GuidePrice   int       `json:"guide_price" gorm:"type:int;not null;"`
+	GuidePhoto   string    `json:"guide_photo" gorm:"type:varchar(500);not null;"`
+	GuideAddress string    `json:"guide_address" gorm:"type:varchar(120);not null;"`
+	GuideContact string    `json:"guide_contact" gorm:"type:varchar(15);not null;"`
+	Price        int       `json:"price" gorm:"type:int;not null;"`
+	CreatedAt    time.Time `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
 }
