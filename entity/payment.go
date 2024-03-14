d29f478d5a9102e8c7f4715814cde88f549f9e99
Christopher Robin <williamchen1506@gmail.com>
2024-03-14 19:20:56 +0700

    refactor : renaming some variables and gorm.Model in Payments struct

--- entity/payment.go
+++ entity/payment.go
@@ -8,9 +8,9 @@ type Payment struct {
 	ID            uuid.UUID `json:"id" gorm:"type:varchar(36);primary_key;"`
 	Amount        int       `json:"total" gorm:"type:int;not null;"`
 	PaymentDate   string    `json:"date" gorm:"type:date;not null;"`
-	TicketID      uuid.UUID `json:"ticketid" gorm:"type:varchar(36);primary_key;foreignkey:ID;references:tickets;onUpdate:CASCADE;onDelete:CASCADE"`
-	GuideID       uuid.UUID `json:"guideid" gorm:"type:varchar(36);primary_key;foreignkey:ID;references:guides;onUpdate:CASCADE;onDelete:CASCADE"`
-	UserID        uuid.UUID `json:"userid" gorm:"type:varchar(36);primary_key;foreignkey:ID;references:users;onUpdate:CASCADE;onDelete:CASCADE"`
+	TicketID      uuid.UUID `json:"ticketid" gorm:"type:varchar(36);foreignkey:ID;references:tickets;onUpdate:CASCADE;onDelete:CASCADE"`
+	GuideID       uuid.UUID `json:"guideid" gorm:"type:varchar(36);foreignkey:ID;references:guides;onUpdate:CASCADE;onDelete:CASCADE"`
+	UserID        uuid.UUID `json:"userid" gorm:"type:varchar(36);foreignkey:ID;references:users;onUpdate:CASCADE;onDelete:CASCADE"`
 	TransactionID uuid.UUID `json:"transactionid" gorm:"type:varchar(36);"`
 	IsPaid        bool      `json:"isPaid" gorm:"default:false"`
 	CreatedAt     string    `json:"createdAt" gorm:"autoCreateTime"`
