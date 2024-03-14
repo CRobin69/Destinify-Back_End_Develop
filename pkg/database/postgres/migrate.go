a3d8e62c42b4bce4d2e0355868fdc4ea8ceaeb87
Christopher Robin <williamchen1506@gmail.com>
2024-03-15 01:59:01 +0700

    refactor : migrate entity City, Place, Ticket, Guide, and Payment

--- pkg/database/postgres/migrate.go
+++ pkg/database/postgres/migrate.go
@@ -9,6 +9,11 @@ import (
 func Migrate(db *gorm.DB) error {
 	if err := db.AutoMigrate(
 		&entity.User{},
+		&entity.City{},
+		&entity.Place{},
+		&entity.Ticket{},
+		&entity.Guide{},
+		&entity.Payment{},
 	); err != nil {
 		return err
 	}
