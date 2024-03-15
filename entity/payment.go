ed5ee0fd990233541958c89dfec525ebf5542f23
Christopher Robin <williamchen1506@gmail.com>
2024-03-15 22:13:47 +0700

    refactor : change data type variabe PaymentDate, CreatedAt, and UpdatedAt

--- entity/payment.go
+++ entity/payment.go
@@ -1,17 +1,19 @@
 package entity
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type Payment struct {
 	ID            uuid.UUID `json:"id" gorm:"type:varchar(36);primary_key;unique;not null;"`
-	OrderID 	  uuid.UUID `json:"orderid" gorm:"type:varchar(36);foreignkey:ID;references:orders;onUpdate:CASCADE;onDelete:CASCADE"`
+	OrderID       uuid.UUID `json:"orderid" gorm:"type:varchar(36);foreignkey:ID;references:orders;onUpdate:CASCADE;onDelete:CASCADE"`
 	Amount        int       `json:"total" gorm:"type:int;not null;"`
-	PaymentDate   string    `json:"date" gorm:"type:date;not null;"`
+	PaymentDate   time.Time `json:"date" gorm:"type:autoCreateTime;not null;"`
 	UserID        uuid.UUID `json:"userid" gorm:"type:varchar(36);foreignkey:ID;references:users;onUpdate:CASCADE;onDelete:CASCADE"`
-	TransactionID uuid.UUID `json:"transactionid" gorm:"type:varchar(36);"`
+	TransactionID uuid.UUID `json:"transactionid" gorm:"type:varchar(36);unique;"`
 	IsPaid        bool      `json:"isPaid" gorm:"default:false"`
-	CreatedAt     string    `json:"createdAt" gorm:"autoCreateTime"`
-	UpdatedAt     string    `json:"updatedAt" gorm:"autoUpdateTime"`
+	CreatedAt     time.Time `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
 }
